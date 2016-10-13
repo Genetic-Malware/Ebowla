bf1fe116e0029b9f7be8c96732368b1091e03d91
secretsquirrel <jhpitts@gmail.com>
2016-10-12 23:05:11 -0400

    System time working

--- templates/powershell/ps_external_ip.py
+++ templates/powershell/ps_external_ip.py
@@ -1,4 +1,22 @@
 buildcode="""
+function Get-ExternalIP(){
+	$extern_ip_mask = @()
+	$response
+	while ($response.IPAddress -eq $null){
+		$response = Resolve-DnsName -Name myip.opendns.com -Server resolver1.opendns.com
+		Start-Sleep -s 1
+
+	}
+	$response.IPAddress
+	$octet1, $octet2, $octet3, $octet4 = $response.IPAddress.Split(".")
+	$octet1, $octet2, $octet3, $octet4
+	$extern_ip_mask += $response.IPAddress
+	$extern_ip_mask += [string]$octet1 + "." + [string]$octet2 + "." + [string]$octet3 + ".0"
+	$extern_ip_mask += [string]$octet1 + "." + [string]$octet2 + ".0.0"
+	$extern_ip_mask += [string]$octet1 + ".0.0.0"
+	return $extern_ip_mask
+}
+	
 """
 
 callcode="""
